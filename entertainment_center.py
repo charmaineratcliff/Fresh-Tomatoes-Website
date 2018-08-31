c0f76e19de771963de8373ded8179e5d7e06bd08
Charmaine Ratcliff <charmaineratcliff@gmail.com>
2018-08-31 17:56:00 -0400

    fix: updated broken trailer link

--- entertainment_center.py
+++ entertainment_center.py
@@ -13,18 +13,18 @@ ferris_bueller = media.Movie("Ferris Bueller's Day Off",
 "https://upload.wikimedia.org/wikipedia/en/9/9b/Ferris_Bueller%27s_Day_Off.jpg",
 "https://www.youtube.com/watch?v=D6gABQFR94U")
 
-forrest_gump = media.Movie("Forrest Gump",
-"https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
+raising_arizona = media.Movie("Raising Arizona",
+"https://upload.wikimedia.org/wikipedia/en/3/31/Raising-Arizona-Poster.jpg",
 "https://www.youtube.com/watch?v=bLvqoHBptjg")
 
 up = media.Movie("Up",
 "https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_film%29.jpg",
-"https://www.youtube.com/watch?v=qas5lWp7_R0")
+"https://www.youtube.com/watch?v=OjWu8i6eMZo")
 
 friday = media.Movie("Friday",
 "http://icecube.com/wp-content/uploads/2015/03/friday-movie-poster.jpg",
 "https://www.youtube.com/watch?v=dxduMVVnrvU")
 
-movies = [coming_to_america, legends_of_the_fall, ferris_bueller, forrest_gump, up, friday]
+movies = [coming_to_america, legends_of_the_fall, ferris_bueller, raising_arizona, up, friday]
 
 fresh_tomatoes.open_movies_page(movies)
